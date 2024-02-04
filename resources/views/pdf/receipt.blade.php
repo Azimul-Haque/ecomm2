72dd0563cc085b780e8736c988d240716be8bc37
Azimul-Haque <orbachinujbuk@gmail.com>
2024-02-05 01:21:53 +0600

    Commit Mon 02/05/2024  1:21:53.25 20983

--- resources/views/pdf/receipt.blade.php
+++ resources/views/pdf/receipt.blade.php
@@ -38,7 +38,7 @@
     <img src="{{ public_path('images/logo.png') }}" style="height: 80px; width: auto;">
     <br/>
     <span class="calibri_normal">Al Amana</span> Halal & Global Food<br/>
-    <small class="calibri_normal" style="font-size: 14px;">Email: sadekshiblu080@gmail.com, Phone: +88 01315 85 25 63</small>
+    <small class="calibri_normal" style="font-size: 14px;">Email: sadekshiblu080@gmail.com, Phone: 080-9212-9030</small>
   </h2>
   <h2 align="center" class="calibri_normal" style="color: #397736; border-bottom: 1px solid #397736;">
     INVOICE
