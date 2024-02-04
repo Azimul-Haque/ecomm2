5e9a4d4753e3c277c3c67f2b94110124c5987e18
Azimul-Haque <orbachinujbuk@gmail.com>
2024-02-05 00:59:11 +0600

    Commit Mon 02/05/2024  0:59:11.30 891

--- resources/views/shop/shoppingcart.blade.php
+++ resources/views/shop/shoppingcart.blade.php
@@ -62,7 +62,7 @@
                               <span id="itemTotalPrice{{ $product['item']['id'] }}" class="">Total: ¥ {{ $product['price'] }}</span>
                             </div>
                             <div class="col-lg-2 col-md-3 col-sm-3 col-xs-12 item-center">
-                              <a class="btn btn-link btn-sm" class="right" href="{{ route('product.removeitem', ['id' => $product['item']['id']]) }}" data-toggle="tooltip" data-placement="bottom" title="এই আইটেমটি তালিকা থেকে বাদ দিতে হলে এখানে ক্লিক করুন"><i class="fa fa-times black-text" aria-hidden="true"></i></a>
+                              <a class="btn btn-link btn-sm" class="right" href="{{ route('product.removeitem', ['id' => $product['item']['id']]) }}" data-toggle="tooltip" data-placement="bottom" title="Click here to remove this item from the list"><i class="fa fa-times black-text" aria-hidden="true"></i></a>
                             </div>
                           </div>
                         </div>
