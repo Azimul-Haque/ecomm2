eb12e9b8b362c1021663c3049d6a12bb12206559
Azimul-Haque <orbachinujbuk@gmail.com>
2024-02-05 01:19:40 +0600

    Commit Mon 02/05/2024  1:19:40.22 23731

--- resources/views/partials/warehousepartials.blade.php
+++ resources/views/partials/warehousepartials.blade.php
@@ -9,12 +9,12 @@
             <i class="fa fa-tachometer" aria-hidden="true"></i> Admin Dashboard<br/> {{ date('F d, Y') }}</button>
           <a href="{{ route('warehouse.dashboard') }}" class="btn btn-default btn-block">Dashboard</a>
           <a href="{{ route('warehouse.dueorders') }}" class="btn btn-default btn-block"><i class="fa fa-list-ol" aria-hidden="true"></i> Pending Orders <span class="label label label-primary">{{ $due_orders }}</span></a>
-          <a href="{{ route('warehouse.deliveredorders') }}" class="btn btn-default btn-block"><i class="fa fa-list-ul" aria-hidden="true"></i> ডেলিভার্ড Orders <span class="label label label-primary">{{ $completed_orders }}</span></a>
-          <a href="{{ route('warehouse.addproduct') }}" class="btn btn-default btn-block"><i class="fa fa-plus" aria-hidden="true"></i> পণ্য যোগ করুণ</a>
-          <a href="{{ route('warehouse.categories') }}" class="btn btn-default btn-block"><i class="fa fa-folder-open-o" aria-hidden="true"></i> পণ্যের শ্রেণীবিভাগ</a>
-          <a href="{{ route('warehouse.customers') }}" class="btn btn-default btn-block"><i class="fa fa-users" aria-hidden="true"></i> কাস্টমার তালিকা</a>
-          <a href="#" class="btn btn-default btn-block"><i class="fa fa-line-chart" aria-hidden="true"></i> রিপোর্ট</a>
-          <a href="#" class="btn btn-default btn-block"><i class="fa fa-sign-out" aria-hidden="true"></i> লগআউট</a>
+          <a href="{{ route('warehouse.deliveredorders') }}" class="btn btn-default btn-block"><i class="fa fa-list-ul" aria-hidden="true"></i> Delivered Orders <span class="label label label-primary">{{ $completed_orders }}</span></a>
+          <a href="{{ route('warehouse.addproduct') }}" class="btn btn-default btn-block"><i class="fa fa-plus" aria-hidden="true"></i> Add Product</a>
+          <a href="{{ route('warehouse.categories') }}" class="btn btn-default btn-block"><i class="fa fa-folder-open-o" aria-hidden="true"></i> Categories</a>
+          <a href="{{ route('warehouse.customers') }}" class="btn btn-default btn-block"><i class="fa fa-users" aria-hidden="true"></i> Customer List</a>
+          <a href="#" class="btn btn-default btn-block"><i class="fa fa-line-chart" aria-hidden="true"></i> Report</a>
+          <a href="#" class="btn btn-default btn-block"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a>
         </div>
       </div>
     </div>
