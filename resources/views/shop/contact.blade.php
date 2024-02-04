bd7df73663a8a035dd545650c023849b6744cd50
Azimul-Haque <orbachinujbuk@gmail.com>
2024-02-05 00:46:38 +0600

    Commit Mon 02/05/2024  0:46:38.24 10994

--- resources/views/shop/contact.blade.php
+++ resources/views/shop/contact.blade.php
@@ -79,7 +79,7 @@
         <div class="container-fuild">
             <div class="row no-margin">
                 <div id="canvas1" class="col-md-12 col-sm-12 no-padding contact-map map">
-                    <iframe id="map_canvas1" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d29198.81699707522!2d90.40015030688478!3d23.823856892923907!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka!5e0!3m2!1sen!2sbd!4v1569511087995!5m2!1sen!2sbd"></iframe>
+                    <iframe id="map_canvas1" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.3625553058273!2d140.16087537634877!3d35.66807383069563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x602285986d0edc81%3A0xa35008cdc737fea9!2s1521-40%20Yotsukaid%C5%8D%2C%20Yotsukaido%2C%20Chiba%20284-0005%2C%20Japan!5e0!3m2!1sen!2sbd!4v1707072359193!5m2!1sen!2sbd"></iframe>
                 </div>
             </div>
         </div>
