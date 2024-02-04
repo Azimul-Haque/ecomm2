d42049b16a38f7ac0c03f8e9ab5f38b7a4aea8a6
Azimul-Haque <orbachinujbuk@gmail.com>
2024-02-05 01:18:57 +0600

    Commit Mon 02/05/2024  1:18:57.30 31554

--- resources/views/partials/warehousepartials.blade.php
+++ resources/views/partials/warehousepartials.blade.php
@@ -8,8 +8,8 @@
           <button type="button" class="btn btn-success btn-block">
             <i class="fa fa-tachometer" aria-hidden="true"></i> Admin Dashboard<br/> {{ date('F d, Y') }}</button>
           <a href="{{ route('warehouse.dashboard') }}" class="btn btn-default btn-block">Dashboard</a>
-          <a href="{{ route('warehouse.dueorders') }}" class="btn btn-default btn-block"><i class="fa fa-list-ol" aria-hidden="true"></i> পেন্ডিং অর্ডারগুলো <span class="label label label-primary">{{ $due_orders }}</span></a>
-          <a href="{{ route('warehouse.deliveredorders') }}" class="btn btn-default btn-block"><i class="fa fa-list-ul" aria-hidden="true"></i> ডেলিভার্ড অর্ডারগুলো <span class="label label label-primary">{{ $completed_orders }}</span></a>
+          <a href="{{ route('warehouse.dueorders') }}" class="btn btn-default btn-block"><i class="fa fa-list-ol" aria-hidden="true"></i> Pending Orders <span class="label label label-primary">{{ $due_orders }}</span></a>
+          <a href="{{ route('warehouse.deliveredorders') }}" class="btn btn-default btn-block"><i class="fa fa-list-ul" aria-hidden="true"></i> ডেলিভার্ড Orders <span class="label label label-primary">{{ $completed_orders }}</span></a>
           <a href="{{ route('warehouse.addproduct') }}" class="btn btn-default btn-block"><i class="fa fa-plus" aria-hidden="true"></i> পণ্য যোগ করুণ</a>
           <a href="{{ route('warehouse.categories') }}" class="btn btn-default btn-block"><i class="fa fa-folder-open-o" aria-hidden="true"></i> পণ্যের শ্রেণীবিভাগ</a>
           <a href="{{ route('warehouse.customers') }}" class="btn btn-default btn-block"><i class="fa fa-users" aria-hidden="true"></i> কাস্টমার তালিকা</a>
