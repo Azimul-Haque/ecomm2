3e7e21368b3be574cf06ebf5c9d0329b54fecdca
Azimul-Haque <orbachinujbuk@gmail.com>
2024-02-05 01:31:20 +0600

    Commit Mon 02/05/2024  1:31:20.31 12158

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -59,7 +59,7 @@
             {!! Form::open(['route' => 'product.checkout', 'method' => 'POST']) !!}
               <ul class="list-group">
                 <li class="list-group-item">
-                  <h4 class="right">ডেলিভারি চার্জঃ ¥ <span id="deliveryCharge">{{ $cart->deliveryCharge }}</span></h4><br/>
+                  <h4 class="right">Delivery Charge ¥ <span id="deliveryCharge">{{ $cart->deliveryCharge }}</span></h4><br/>
                 </li>
                 @if(Auth::user()->points > 0)
                   <li class="list-group-item">
