8386c24004e3832099f936c7aaf32685de6113dc
Azimul-Haque <orbachinujbuk@gmail.com>
2024-02-05 01:56:19 +0600

    Commit Mon 02/05/2024  1:56:19.47 29175

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -141,7 +141,7 @@
             <br/>
             <div class="panel panel-primary shadow-light">
               <div class="panel-heading">
-                <h4 class="panel-title">আপনার পূর্ববর্তী অর্ডারগুলো</h4>
+                <h4 class="panel-title">Your previous orders</h4>
               </div>
               <div class="panel-body">
                 <div class="panel-group" id="accordion">
