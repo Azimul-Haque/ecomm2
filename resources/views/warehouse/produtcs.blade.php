8c8ca736654794b0298b3cb1e6a1bc3b092af01c
Azimul-Haque <orbachinujbuk@gmail.com>
2024-03-17 04:13:49 +0600

    Commit Sun 03/17/2024  4:13:49.26 9840

--- resources/views/warehouse/produtcs.blade.php
+++ resources/views/warehouse/produtcs.blade.php
@@ -71,6 +71,7 @@
                     </td>
                     <td>
                       <img style="max-height: 40px; border:1px solid #777" class="img-responsive" src="{{ asset('images/product-images/'.$product->productimages->first()->image) }}">
+                      {{ $product->productimages->first()->image }}
                     </td>
                     <td>{{ $product->stock }}</td>
                     <td>৳ {{ $product->buying_price }}</td>
